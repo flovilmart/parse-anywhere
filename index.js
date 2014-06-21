1b5a3d0668254d69f3783302b6fb790963d52e60
sodas tsai <sodas2002@gmail.com>
2014-06-21 15:18:24 +0800

    Refine/Reformat code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+/*jshint node:true */
 
-
-Parse = require("parse-cloud-additions").Parse;
-
+// Load Parse
+var Parse = require("parse-cloud-additions").Parse;
 global.Parse = Parse;
 
 var appName = process.argv[3];
@@ -9,26 +9,26 @@ if (appName && appName !== "undefined") {
     console.log("Starting app "+appName);
 }
 
-var start = function(config){
+var start = function(config) {
+    'use strict';
+
     console.log("[%s] Running...", process.pid);
-    Parse.initialize(config.applicationId,config.javascriptKey,config.masterKey);
+    Parse.initialize(config.applicationId, config.javascriptKey, config.masterKey);
+
     require("cloud/main");
-}
-// 4 is the stringified config file, passed from previous script...
+};
+
+// 4 is the stringified config file, passed from previous script (i.e. parse-develop)
 if (process.argv[4]) {
     start(JSON.parse(process.argv[4]));
-}else{
+} else {
     var path = process.argv[2];
-    require("./lib/utils").getConfiguration(path, appName, function(err, config){
+    require("./lib/utils").getConfiguration(path, appName, function(err, config) {
+        'use strict';
         if (err) {
             return console.error(err);
-        }else{
+        } else {
             start(config);
         }
     });
 }
-
-
-
-
-
